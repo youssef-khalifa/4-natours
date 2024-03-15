883cd38146cfff2866b71243f02325efb4ee2884
youssef-khalifa <ykhalifa19921@gmail.com>
2024-03-15 18:05:55 +0200

    unhandled routes + implemeting ndb debugger

--- starter/app.js
+++ starter/app.js
@@ -18,16 +18,18 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('helooo from the middleware');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 app.use('/api/v1/tours', tourRourter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*',(req,res,next)=>{
+  res.status(404).json({
+    status:'fail',
+    message:`Cant find ${req.originalUrl} on this server`
+  })
+})
 
 module.exports = app;
