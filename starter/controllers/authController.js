eb8cb55afd2609da6206b7d4468de35ee725c39c
youssef-khalifa <ykhalifa19921@gmail.com>
2024-03-23 07:03:08 +0200

    protecting tour routes using jwt

--- starter/controllers/authController.js
+++ starter/controllers/authController.js
@@ -1,3 +1,4 @@
+const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 const AppErorr = require('./../utils/appError');
 const User = require('./../models/userModel');
@@ -15,6 +16,7 @@ exports.signup = catchAsynch(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
+    passwordChangedAt: req.body.passwordChangedAt,
   });
   const token = signToken(newUser._id);
 
@@ -54,18 +56,33 @@ exports.protect = catchAsynch(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
-    token = req.headers.authorization.split(' ')[0];
+    token = req.headers.authorization.split(' ')[1];
   }
-  console.log(req.headers);
   if (!token) {
     return next(
       new AppErorr('you are not logged in ! please log in to get access', 401)
     );
   }
   //2 verfication token
+  const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
   //3 check if user still exist
-
+  const freshUser = await User.findById(decoded.id);
+  if (!freshUser) {
+    return next(
+      new AppErorr(
+        'the user belonging to this token does no longer exist.',
+        401
+      )
+    );
+  }
   //4 check if user changed pass after token was issued
+  if (freshUser.changedPasswordAfter(decoded.iat)) {
+    return next(
+      new AppErorr('use recently changed password! please login again.', 401)
+    );
+  }
+  // grant access to protected route
+  req.user = freshUser;
   next();
 });
--- starter/controllers/errorController.js
+++ starter/controllers/errorController.js
@@ -16,7 +16,8 @@ const handleValidationErrorDB = (err) => {
   const message = `Invalid input data. ${errors.join('. ')}`;
   return new AppError(message, 400);
 };
-
+const handleJWTError=()=>new AppError('invalid token please login again',401)
+const handleJWTExpiredError=()=> new AppError('token expired please login again',401)
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -60,7 +61,8 @@ module.exports = (err, req, res, next) => {
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
-
+    if(error.name==='JsonWebTokenError') error= handleJWTError()
+    if(error.name==='TokenExpiredError') error=handleJWTExpiredError()
     sendErrorProd(error, res);
   }
 };
