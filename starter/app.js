bce4359ad958672227a9cb2750f9bf29bd5e1f51
youssef-khalifa <ykhalifa19921@gmail.com>
2024-03-26 10:48:05 +0200

    .

--- starter/app.js
+++ starter/app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
-const helmet =require('helmet')
+const helmet = require('helmet');
+const monoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 const rateLimit = require('express-rate-limit');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -9,9 +12,10 @@ dotenv.config({ path: './config.env' });
 
 const tourRourter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const ExpressMongoSanitize = require('express-mongo-sanitize');
 
 const app = express();
-app.use(helmet())
+app.use(helmet());
 
 // 1- middlewares
 if (process.env.NODE_ENV === 'development') {
@@ -21,10 +25,26 @@ if (process.env.NODE_ENV === 'development') {
 const limiter = rateLimit({
   max: 1000,
   windowMs: 60 * 60 * 1000,
-  message:'too many requests from this ip,please try agai in 1 hour'
+  message: 'too many requests from this ip,please try agai in 1 hour',
 });
-app.use('/api',limiter)
-app.use(express.json({limit:'100kb'}));
+app.use('/api', limiter);
+app.use(express.json({ limit: '100kb' }));
+app.use(monoSanitize());
+app.use(xss());
+
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
