df994f33a5ca95fb183a81ce3bd554b59984b7c7
youssef-khalifa <ykhalifa19921@gmail.com>
2024-04-02 08:33:23 +0200

    building the entire front using pug templates (alot of detailes but im just gonna say that )*tired vibes*

--- starter/app.js
+++ starter/app.js
@@ -1,36 +1,58 @@
+const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
-const dotenv = require('dotenv');
+const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const dotenv = require('dotenv');
 const monoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
-const rateLimit = require('express-rate-limit');
+const cookieParser = require('cookie-parser');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
-dotenv.config({ path: './config.env' });
-
 const tourRourter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
-const app = express();
-app.use(helmet());
+const viewRouter = require('./routes/viewRoutes');
 
-// 1- middlewares
+dotenv.config({ path: './config.env' });
+
+const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+// 1) GLOBAL MIDDLEWARES
+// Serving static files
+app.use(express.static(path.join(__dirname, 'public')));
+// Set security HTTP headers
+
+// app.use(helmet());
+app.use(helmet({ contentSecurityPolicy: false }));
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
+// Limit requests from same API
 const limiter = rateLimit({
   max: 1000,
   windowMs: 60 * 60 * 1000,
   message: 'too many requests from this ip,please try agai in 1 hour',
 });
 app.use('/api', limiter);
-app.use(express.json({ limit: '100kb' }));
+// Body parser, reading data from body into req.body
+
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+app.use(cookieParser());
+
+// Data sanitization against NoSQL query injection
 app.use(monoSanitize());
+
+// Data sanitization against XSS
 app.use(xss());
 
+// Prevent parameter pollution
 app.use(
   hpp({
     whitelist: [
@@ -44,16 +66,18 @@ app.use(
   })
 );
 
-app.use(express.static(`${__dirname}/public`));
-
+// Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
+  console.log(req.cookies)
   next();
 });
-
+// 3) ROUTES
+app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRourter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+
 app.all('*', (req, res, next) => {
   next(new AppError(`Cant find ${req.originalUrl} on this server!`, 404));
 });
--- starter/controllers/authController.js
+++ starter/controllers/authController.js
@@ -39,8 +39,6 @@ exports.signup = catchAsynch(async (req, res, next) => {
     email: req.body.email,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
-    passwordChangedAt: req.body.passwordChangedAt,
-    role: req.body.role,
   });
   createSendToken(newUser, 201, res);
 });
@@ -61,6 +59,13 @@ exports.login = catchAsynch(async (req, res, next) => {
   createSendToken(user, 200, res);
 });
 
+exports.logout = (req, res) => {
+  res.cookie('jwt', 'loggedout', {
+    expires: new Date(Date.now() + 10 * 1000),
+    httpOnly: true
+  });
+  res.status(200).json({ status: 'success' });
+};
 exports.protect = catchAsynch(async (req, res, next) => {
   let token;
   // 1) Getting token and check of it's there
@@ -69,6 +74,8 @@ exports.protect = catchAsynch(async (req, res, next) => {
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
+  } else if (req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
   if (!token) {
     return next(
@@ -96,8 +103,40 @@ exports.protect = catchAsynch(async (req, res, next) => {
   }
   // GRANT ACCESS TO PROTECTED ROUTE
   req.user = currentUser;
+  res.locals.user = currentUser;
   next();
 });
+
+exports.isLoggedIn = async (req, res, next) => {
+  if (req.cookies.jwt) {
+    try {
+      // 1) verify token
+      const decoded = await promisify(jwt.verify)(
+        req.cookies.jwt,
+        process.env.JWT_SECRET
+      );
+
+      // 2) Check if user still exists
+      const currentUser = await User.findById(decoded.id);
+      if (!currentUser) {
+        return next();
+      }
+
+      // 3) Check if user changed password after the token was issued
+      if (currentUser.changedPasswordAfter(decoded.iat)) {
+        return next();
+      }
+
+      // THERE IS A LOGGED IN USER
+      res.locals.user = currentUser;
+      return next();
+    } catch (err) {
+      return next();
+    }
+  }
+  next();
+};
+
 
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
