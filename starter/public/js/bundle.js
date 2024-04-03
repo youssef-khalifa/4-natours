431ebd0b91f5e1c6071a600dbccdde235f6aa76f
youssef-khalifa <ykhalifa19921@gmail.com>
2024-04-03 02:37:50 +0200

    renderig error pages

--- starter/public/js/bundle.js
+++ starter/public/js/bundle.js
@@ -12383,7 +12383,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54233" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57154" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
